64b3eb4c8ba6bdd5f866596e454e2da8151f1169
uzAndijan <uzguitarist98@gmail.com>
2023-07-12 08:27:11 +0200

    second commit

--- AndersenGit/src/Main.java
+++ AndersenGit/src/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
 
+        System.out.println("Hello, world!");
 
         System.out.println("Hello, world!");
 
-        
-
-
-
     }
 }
+
+
+
+
